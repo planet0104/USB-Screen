b8a56657f24825827e3b94c5c25954ac85893efe
JiaYe <planet2@qq.com>
2024-05-25 22:33:42 +0800

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 cargo zbuild --target x86_64-pc-windows-msvc
-cargo zbuild --target x86_64-unknown-linux-gnu
-cargo zbuild --target i686-unknown-linux-gnu
-cargo zbuild --target aarch64-unknown-linux-gnu
+@REM cargo zbuild --target x86_64-unknown-linux-gnu
+@REM cargo zbuild --target i686-unknown-linux-gnu
+@REM cargo zbuild --target aarch64-unknown-linux-gnu
